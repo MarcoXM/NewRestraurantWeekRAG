171710aea61fb86fb08de6bccf36a7cff1139212
Marco Wang <xwang423@fordham.edu>
2024-12-30 13:49:00 -0500

    update api out put

--- app/myrag.py
+++ app/myrag.py
@@ -12,21 +12,22 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 from models import QueryResult
 
 # Set the OCR_AGENT environment variable
-os.environ['OCR_AGENT'] = 'pytesseract'
+os.environ["OCR_AGENT"] = "pytesseract"
 
 IS_USING_IMAGE_RUNTIME = bool(os.getenv("IS_USING_IMAGE_RUNTIME", False))
 
+
 def sanitize_string(input_string):
     # Remove special characters
-    sanitized = re.sub(r'[^a-zA-Z0-9\s]', '', input_string.strip())
+    sanitized = re.sub(r"[^a-zA-Z0-9\s]", "", input_string.strip())
     # Convert to lowercase
     sanitized = sanitized.lower()
     # Replace spaces with hyphens
-    sanitized = sanitized.replace(' ', '-')
+    sanitized = sanitized.replace(" ", "-")
     return sanitized
 
 
@@ -37,8 +38,8 @@ def download_pdfs_from_csv(csv_path, download_folder):
     # Iterate over each row in the DataFrame
     name_2_path = {}
     for index, row in df.iterrows():
-        restaurant_name = row['headline']
-        menu_url = row['menu_url']
+        restaurant_name = row["headline"]
+        menu_url = row["menu_url"]
 
         # Generate a sanitized file name
         file_name = f"{sanitize_string(restaurant_name)}.pdf"
@@ -50,19 +51,20 @@ def download_pdfs_from_csv(csv_path, download_folder):
             response.raise_for_status()  # Check if the request was successful
 
             # Save the PDF to the specified folder
-            with open(file_path, 'wb') as file:
+            with open(file_path, "wb") as file:
                 file.write(response.content)
             print(f"Downloaded {file_name} successfully.")
             name_2_path[restaurant_name] = file_path
         except requests.exceptions.RequestException as e:
             print(f"Failed to download {file_name}: {e}")
 
-    df['file_path'] = df['headline'].map(name_2_path)
-    df.to_csv('restaurant_menu_pdf.csv', index=False)
+    df["file_path"] = df["headline"].map(name_2_path)
+    df.to_csv("restaurant_menu_pdf.csv", index=False)
+
 
 # Example usage
-csv_path = 'app/restaurant_menu_urls.csv'  # Path to your CSV file
-download_folder = 'menu_urls'  # Folder to save the downloaded PDFs
+csv_path = "app/restaurant_menu_urls.csv"  # Path to your CSV file
+download_folder = "menu_urls"  # Folder to save the downloaded PDFs
 
 
 if not os.path.exists(download_folder) and not IS_USING_IMAGE_RUNTIME:
@@ -76,11 +78,8 @@ else:
     print(f"Folder already exists: {download_folder}")
 
 
-
-
 embeddings = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
 persist_directory = "./db"
-
 
 
 def get_runtime_chroma_path():
@@ -89,7 +88,8 @@ def get_runtime_chroma_path():
     else:
         return persist_directory
 
-def copy_chroma_to_tmp(CHROMA_PATH= persist_directory):
+
+def copy_chroma_to_tmp(CHROMA_PATH=persist_directory):
     dst_chroma_path = get_runtime_chroma_path()
 
     if not os.path.exists(dst_chroma_path):
@@ -113,11 +113,13 @@ if os.path.exists(persist_directory) and os.listdir(persist_directory):
         __import__("pysqlite3")
         sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
-        # move the file to /tmp and return the new path 
+        # move the file to /tmp and return the new path
         persist_directory = copy_chroma_to_tmp()
-    
+
     print("Loading existing vectorstore...")
-    vectorstore = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+    vectorstore = Chroma(
+        persist_directory=persist_directory, embedding_function=embeddings
+    )
 else:
     print("Creating new vectorstore...")
     # Load PDFs using langchain_community.document_loaders
@@ -125,7 +127,7 @@ else:
 
     all_documents = []
 
-    df = pd.read_csv('restaurant_menu_pdf.csv')
+    df = pd.read_csv("restaurant_menu_pdf.csv")
 
     for data in df.to_dict(orient="records"):
         fn = data["file_path"]
@@ -144,18 +146,18 @@ else:
         print("Splitting text...")
         text_splitter = RecursiveCharacterTextSplitter(
             separators=[
-            "\n\n",
-            "\n",
-            " ",
-            ".",
-            ",",
-            "\u200b",  # Zero-width space
-            "\uff0c",  # Fullwidth comma
-            "\u3001",  # Ideographic comma
-            "\uff0e",  # Fullwidth full stop
-            "\u3002",  # Ideographic full stop
-            "",
-        ],
+                "\n\n",
+                "\n",
+                " ",
+                ".",
+                ",",
+                "\u200b",  # Zero-width space
+                "\uff0c",  # Fullwidth comma
+                "\u3001",  # Ideographic comma
+                "\uff0e",  # Fullwidth full stop
+                "\u3002",  # Ideographic full stop
+                "",
+            ],
             chunk_size=800,
             chunk_overlap=100,
             length_function=len,
@@ -166,17 +168,19 @@ else:
         for doc in documents:
 
             meta_dict = {
-                    'cuisine' : cuisine.strip(),
-                    'restaurant_name' : restaurant_name.strip(),
-                    'location' : location.strip(),
-                }
+                "cuisine": cuisine.strip(),
+                "restaurant_name": restaurant_name.strip(),
+                "location": location.strip(),
+            }
             doc.metadata = meta_dict
-            
+
             doc.page_content += str(meta_dict) + "\n"
 
         all_documents.extend(documents)
 
-    vectorstore = Chroma.from_documents(all_documents, embeddings, persist_directory=persist_directory)
+    vectorstore = Chroma.from_documents(
+        all_documents, embeddings, persist_directory=persist_directory
+    )
     if not os.path.exists(persist_directory) or not os.listdir(persist_directory):
         vectorstore.persist()
 
@@ -187,10 +191,9 @@ from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 prompt = hub.pull("rlm/rag-prompt")
 
+
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
-
-
 
 
 from langchain_core.prompts import PromptTemplate
@@ -234,34 +237,43 @@ Question: {question}
 Helpful Answer:"""
 custom_rag_prompt = PromptTemplate.from_template(template)
 
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+rag_chain_from_docs = (
+    RunnablePassthrough.assign(context=(lambda x: format_docs(x["context"])))
     | custom_rag_prompt
     | llm
     | StrOutputParser()
 )
 
+rag_chain_with_source = RunnableParallel(
+    {"context": retriever, "question": RunnablePassthrough()}
+).assign(answer=rag_chain_from_docs)
+
 
 # Function to ask questions
 def ask_question(question):
     print("Answer:\n\n", end=" ", flush=True)
-    ans = ""
-    for chunk in rag_chain.stream(question):
-        print(chunk, end="", flush=True)
-        ans += chunk
-    print("\n")
+    ans = rag_chain_with_source.invoke(question)
+
     return ans
 
+
 def query_rag(query_text):
+    # Get the answer and fill in the QueryResult object
     answer = ask_question(query_text)
-    return QueryResult(query_text=query_text, answer_text=answer, sources=[],is_complete=True)
+
+    return QueryResult(
+        query_text=query_text,
+        answer_text=answer.get("answer"),
+        sources=[x.page_content for x in answer.get("context") if x.page_content],
+        is_complete=True,
+    )
 
 
 # Example usage
 if __name__ == "__main__":
     while True:
         user_question = input("Ask a question (or type 'quit' to exit): ")
-        if user_question.lower() == 'quit':
+        if user_question.lower() == "quit":
             break
         answer = ask_question(user_question)
         # print("\nFull answer received.\n")
